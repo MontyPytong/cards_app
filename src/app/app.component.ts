c65f138cd6531b0f893b2666593bff026b18ec54
Cristi Ene <ene.georgec@gmail.com>
2023-04-10 23:34:08 +0300

    cards app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards_app';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl:'assets/tree.jpeg',
+      username:'@nature',
+      content:'Saw this awsome tree today'
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl:'assets/mountain.jpeg',
+      username:'@mountainlover',
+      content:'Here is a picture of a snowy mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl:'assets/biking.jpeg',
+      username:'@enjoybiking',
+      content:'Look at my cool byke'
+    },
+    
+  ];
 }
